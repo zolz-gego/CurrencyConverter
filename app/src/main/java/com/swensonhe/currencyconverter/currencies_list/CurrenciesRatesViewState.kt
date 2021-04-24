445d76b0ded23a96dc73efe48b30d390d5b32ecc
George <george@trella.app>
2021-04-24 15:23:57 +0200

    Add CurrenciesRatesViewState.kt

---
+++ app/src/main/java/com/swensonhe/currencyconverter/currencies_list/CurrenciesRatesViewState.kt
@@ -0,0 +1,10 @@
+package com.swensonhe.currencyconverter.currencies_list
+
+/**
+ * Created by George on 4/24/21.
+ */
+data class CurrenciesRatesViewState(
+    val isLoading: Boolean = false,
+    val error: String? = null,
+    val rates: HashMap<String, Float>?
+)
