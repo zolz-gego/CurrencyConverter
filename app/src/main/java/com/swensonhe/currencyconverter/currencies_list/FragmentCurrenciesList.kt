a3383a7ddcddf11c84060b892a24aa5c890d1eec
George <george@trella.app>
2021-04-24 14:49:44 +0200

    Adding item_currency.xml

--- app/src/main/java/com/swensonhe/currencyconverter/currencies_list/FragmentCurrenciesList.kt
+++ app/src/main/java/com/swensonhe/currencyconverter/currencies_list/FragmentCurrenciesList.kt
@@ -12,14 +12,14 @@ import com.swensonhe.currencyconverter.R
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class FragmentCurrenciesList : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_currencies_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
