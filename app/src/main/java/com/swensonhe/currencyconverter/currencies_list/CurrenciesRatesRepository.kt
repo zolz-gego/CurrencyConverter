f270320e5c73a3d66203b006f0bdbdf96dc9d744
George <george@trella.app>
2021-04-24 15:10:42 +0200

    calling get currencies api

--- app/src/main/java/com/swensonhe/currencyconverter/currencies_list/CurrenciesRatesRepository.kt
+++ app/src/main/java/com/swensonhe/currencyconverter/currencies_list/CurrenciesRatesRepository.kt
@@ -1,7 +1,13 @@
 package com.swensonhe.currencyconverter.currencies_list
 
+import com.swensonhe.currencyconverter.utilities.FixerIoAPI
+
 /**
  * Created by George on 4/24/21.
  */
-class CurrenciesRatesRepository {
+class CurrenciesRatesRepository(private val api: FixerIoAPI) {
+
+    suspend fun getCurrenciesRates(baseCurrency: String) {
+        api.getCurrencies(baseCurrency = baseCurrency)
+    }
 }
